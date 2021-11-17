d828676edee88c5f6594c96c3155b18c1ee302e6
atsuki-takahashi180090 <tejc180090@gmail.com>
2021-11-17 16:24:51 +0900

    単体テスト

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,11 +1,89 @@
 from flaskr import create_app
+import pytest
+import json
+
 
 def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
 
-def test_hello():
+#def test_hello():
+#   app = create_app({'TESTING': True})
+#    client = app.test_client() 
+#    response = client.get('/hello')
+#    assert response.data == b'Hello, World!'
+
+
+
+
+
+def test_200_temperatureSense():
     app = create_app({'TESTING': True})
+
     client = app.test_client() 
-    response = client.get('/hello')
-    assert response.data == b'Hello, World!'
+    response = client.get('/temperatureSense?tSense=10')
+    data = json.loads(response.data)
+    assert data["status"] == "200 OK"
+
+
+def test_204_temperatureSense():
+    app = create_app({'TESTING': True})
+
+    client = app.test_client() 
+    response = client.get('/temperatureSense')
+    data = json.loads(response.data)
+    assert data["status"] == "204 No Content"
+
+
+#################################################################
+
+def test_200_temperatureActual():
+    app = create_app({'TESTING': True})
+
+    client = app.test_client() 
+    response = client.get('/temperatureActual?sNumber=10&tActual=555')
+    data = json.loads(response.data)
+    assert data["status"] == "200 OK"
+
+
+def test_204_temperatureActual():
+    app = create_app({'TESTING': True})
+
+    client = app.test_client() 
+    response = client.get('/temperatureActual')
+    data = json.loads(response.data)
+    assert data["status"] == "204 No Content"
+
+
+def test_400_temperatureActual():
+    app = create_app({'TESTING': True})
+
+    client = app.test_client() 
+    response = client.get('/temperatureActual?sNumber=&tActual=')
+    data = json.loads(response.data)
+    assert data["status"] == "400 Bad Request"
+
+
+
+
+####################################################################
+
+def test_200_postNumber():
+    app = create_app({'TESTING': True})
+
+    client = app.test_client() 
+    response = client.get('/postNumber?pNumber=960-8253')
+    data = json.loads(response.data)
+    assert data["status"] == "200 OK"
+
+
+def test_204_postNumber():
+    app = create_app({'TESTING': True})
+
+    client = app.test_client() 
+    response = client.get('/postNumber')
+    data = json.loads(response.data)
+    assert data["status"] == "204 No Content"
+
+
+
