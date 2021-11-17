238fa14725d2b1699727f89f0da2a8713322ce21
Naoki Sasaki <tejc18074@gmail.com>
2021-11-17 16:19:18 +0900

    外気温取得機能実装

---
+++ tests/test_SerchTemperature.py
@@ -0,0 +1,9 @@
+from flaskr.util.SearchTemperature import get_temperature
+#input:郵便番号、output:float型の数値
+def test_get_temperature():
+    temperature = get_temperature("980-0021")
+    assert isinstance(temperature, float) == True
+
+def test_get_temperature_NGcase():
+    temperature = get_temperature("980-0021")
+    assert isinstance(temperature, int) == False
