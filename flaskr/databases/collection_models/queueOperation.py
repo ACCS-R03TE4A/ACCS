23ed33a61863de7accaeb2e06cc982a7823dbafb
atsuki-takahashi180090 <tejc180090@gmail.com>
2021-12-06 10:04:31 +0900

    _id指定の削除

--- flaskr/databases/collection_models/queueOperation.py
+++ flaskr/databases/collection_models/queueOperation.py
@@ -1,7 +1,6 @@
 from mongoengine import connect, Document, StringField, IntField, BinaryField,ObjectIdField
 
 class queueOperation(Document):
-    _id = ObjectIdField(Required=True, primary_key=True)
     appliance = StringField(Required=True)
     protocol = IntField(Required=True)
     data = BinaryField(Required=True)
@@ -12,16 +11,13 @@ class queueOperation(Document):
     meta = {'collection':'queueOperation'}
 
     def __str__(self):
-        return f"{self._id}, {self.appliance}, {self.protocol}, {self.data}, {self.size}"
-        
-        #
-        # ope_list = [{self._id}, {self.appliance}, {self.protocol}, {self.data}, {self.size}]
-        # return ope_list
+        return f"{self.appliance}, {self.protocol}, {self.data}, {self.size}"
+    
 
     
     def get_dict(self):
 
-        ope_dict = {"_id":str(self._id), "appliance":self.appliance, 
+        ope_dict = {"appliance":self.appliance, 
         "protocol":self.protocol, "data":int(self.data), 
         "size":self.size}
 
