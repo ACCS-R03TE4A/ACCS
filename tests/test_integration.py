d2a2bf8f9a2bdf468a54a5984d8a55b91b3e0fd3
atsuki-takahashi180090 <tejc180090@gmail.com>
2021-12-07 12:32:52 +0900

    test1/5

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,25 +1,29 @@
-from flaskr import create_app
-import pytest
 import json
 
-
-from flaskr import create_app
 import pytest
-import json
+from flaskr import create_app
+from flaskr.databases.collection_models.queueOperation import queueOperation
+
+# def test_config():
+#     assert not create_app().testingassert
+#     create_app({'TESTING': True}).testing
 
 
-def test_config():
-    assert not create_app().testing
-    assert create_app({'TESTING': True}).testing
-
-
-def test_tSense_1():
+def test_tSense_0():
     app = create_app({'TESTING': True})
     client = app.test_client() 
 
-    response = client.get('/temperatureSense?tSense=1')
-    data = json.loads(response.data)
-    assert data["status"] == "200 OK"  #temperatureSense
-    #assert
+    #queueOperationを空にする
+    queueOperation.objects.all().delete()
+
+    client.get('/temperatureSense?tSense=0')
+    operation = queueOperation.objects(appliance="学校のサーキュレータ").order_by("+_id").first()
+    x = int.from_bytes(operation.data, "big")
+    ope = hex(x)
+    
+    operation.delete()
+
+    assert ope == "0x17b00ff"
 
 
+#################################################################
