f427a187d3bf4b6530f36df5c31762eb0b723bd6
atsuki-takahashi180090 <tejc180090@gmail.com>
2021-12-07 10:57:28 +0900

    余計なコメントや行の削除

--- flaskr/apis/operation.py
+++ flaskr/apis/operation.py
@@ -6,21 +6,10 @@ import traceback
 import json
 import binascii
 
-#確認用##################
 def digitConversion(x):
-
     y = int.from_bytes(x, "big")
     z = hex(y)
-
-    #y = str (x)
-    #z = x.hex()
-
-    #print(z)
     return z
-
-
-#########################
-
 
 #{status:0 or 0以外}で指示があるかないかを判別させる
 #status = データベースの要素数
@@ -43,31 +32,24 @@ def get_data():
 
         #ObjectIdは作成タイムスタンプをカプセル化するため、ObjectIdで直接並べ替えることができます。
         #これは、作成日時によって自然に並べ替えられます。
-        
         if d != 0:
             operation = queueOperation.objects(appliance="学校のサーキュレータ").order_by("+_id").first()
 
             dict_ope = operation.get_dict()
             print(dict_ope)
 
-            #確認
+            #16進数への変換
             x = digitConversion(operation.data)
-
             dict_ope["data"] = x
-
             print(dict_ope)
-
-
-            
+            #
 
             operation.delete()
             return {"status":d, "operation":dict_ope}
-
         
         else:
             return{"status":d}
-    
-
+        
     except Exception as e:
         traceback.print_exc() #エラー
         return {"status":"400 Bad Request"}
