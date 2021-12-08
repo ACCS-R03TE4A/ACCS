7fd9f47eb8127112d8bbe6d7e5edc41be567b4aa
atsuki-takahashi180090 <tejc180090@gmail.com>
2021-12-08 16:33:56 +0900

    submoduleの更新

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -2,6 +2,8 @@
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
+git submodule init
+git submodule update
 pytest
 ssh 192.168.1.83 "rm -rf ACCS-SERVER"
 ssh 192.168.1.83 "git clone https://github.com/ACCS-R03TE4A/ACCS-SERVER.git"
