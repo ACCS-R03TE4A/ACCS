42c5a6321f2f505c9984109d5627b185f4d5bef0
te180453 <te180453jc21@gmail.com>
2021-12-08 15:04:21 +0900

    configでbuildの種類を選択できるようにした。

--- flaskr/config.py
+++ flaskr/config.py
@@ -1,4 +1,13 @@
 import os
+# BUILD : "release" | "debug" | "test"
+BUILD = "test"
 
-DATABASE_CONNECTINO_STRING = f"mongodb://accs:{os.environ['mongodb_password']}@192.168.1.111:27017/ACCS"
-DATABASE_NAME = "ACCS"
+if BUILD == "test":
+    DATABASE_NAME = "ACCS_TEST"
+elif BUILD == "debug":
+    DATABASE_NAME = "ACCS_DEBUG"
+elif BUILD == "release":
+    DATABASE_NAME = "ACCS_RELEASE"
+#----------COMMON----------
+DATABASE_CONNECTINO_STRING = f"mongodb://accs:{os.environ['mongodb_password']}@192.168.1.111:27017/{DATABASE_NAME}"
+#------------------------------
