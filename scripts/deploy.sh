a449976037a32e8314a830e0a2dec0772d170f58
te180453 <83932714+te180453@users.noreply.github.com>
2021-12-17 11:26:29 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,9 @@ git submodule update
 export FLASK_APP=flaskr
 export FLASK_ENV=development
 mongorestore --db="ACCS" dump/ACCS_DUMP/ 
+git clone https://github.com/ACCS-R03TE4A/Remote-control-app.git
+cd Remote-control-app
+npm install
+npm run build
+cp -r build ../resources/
 flask run -h 0.0.0.0
