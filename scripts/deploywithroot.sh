ae7b3f3c875b841f92a98c57aaf567e4e5b9168d
te180453 <83932714+te180453@users.noreply.github.com>
2021-12-17 10:27:58 +0900

    Update deploywithroot.sh

--- scripts/deploywithroot.sh
+++ scripts/deploywithroot.sh
@@ -7,5 +7,7 @@ wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 apt-get update
 apt-get install -y mongodb-org
+wget -qO - https://deb.nodesource.com/setup_lts.x | bash -
+apt install -y nodejs
 systemctl start mongod
 systemctl enable mongod
