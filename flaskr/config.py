c0ac5c0f7a8f5daff44c37a9ddbee18845289d7a
te180453 <te180453jc21@gmail.com>
2021-12-20 12:22:37 +0900

    update

--- flaskr/config.py
+++ flaskr/config.py
@@ -10,4 +10,7 @@ elif BUILD == "release":
     DATABASE_NAME = "ACCS_RELEASE"
 #----------COMMON----------
 DATABASE_CONNECTINO_STRING = f"mongodb://localhost:27017/{DATABASE_NAME}"
+REMOCON_CONFIG_PATH = "Remote-control-app/src/AppConfig.json"
+REMOCON_APP_PATH = "Remote-control-app"
+RESOURCES_PATH = "resources"
 #------------------------------
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,8 +8,3 @@ export FLASK_APP=flaskr
 export FLASK_ENV=development
 mongorestore --db="ACCS" dump/ACCS_DUMP/ 
 git clone https://github.com/ACCS-R03TE4A/Remote-control-app.git
-cd Remote-control-app
-npm install
-npm run build
-cp -r build ../resources/
-flask run -h 0.0.0.0
