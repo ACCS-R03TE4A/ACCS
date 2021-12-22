13df4014f018e35a159a2b3022922004da792b25
te180453 <te180453jc21@gmail.com>
2021-12-22 10:36:35 +0900

    デプロイコードのコメント化

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -4,7 +4,10 @@ source env/bin/activate
 pip install -r requirements.txt
 git submodule init
 git submodule update
+export FLASK_APP=flaskr
+export FLASK_ENV=test
+mongorestore --db="ACCS" dump/ACCS_DUMP/ 
 pytest
-ssh 192.168.1.83 "rm -rf ACCS-SERVER"
-ssh 192.168.1.83 "git clone https://github.com/ACCS-R03TE4A/ACCS-SERVER.git"
-ssh 192.168.1.83 "source ~/.profile;cd ACCS-SERVER;chmod +x ./scripts/deploy.sh;./scripts/deploy_test.sh"
+# ssh 192.168.1.83 "rm -rf ACCS-SERVER"
+# ssh 192.168.1.83 "git clone https://github.com/ACCS-R03TE4A/ACCS-SERVER.git"
+# ssh 192.168.1.83 "source ~/.profile;cd ACCS-SERVER;chmod +x ./scripts/deploy.sh;./scripts/deploy_test.sh"
