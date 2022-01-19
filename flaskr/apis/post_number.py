dd6d1ad2967fd4b836bf0101add7c129cea4a387
atsuki-takahashi180090 <tejc180090@gmail.com>
2022-01-19 16:03:00 +0900

    郵便番号の有無で 保存 or 参照を行うようにした。

--- flaskr/apis/post_number.py
+++ flaskr/apis/post_number.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, Response
 from flaskr.app import app
 # from flaskr.databases.db import db
 from flaskr.databases.collection_models.setting import Setting
@@ -9,20 +9,26 @@ logger = getLogger(__name__)
 with open("log_config.json", "r") as f:
     config.dictConfig(json.load(f))
 
+
 #リモコンアプリからの郵便番号
+#requestじゃない呼び出し方というか、直接メソッドとして呼ばれたときにDBに保存してある郵便番号を返す
+#追加や変更は    requests.get(f"HTTP://localhost:5000/postNumber?pNumber=○○○○○○○
+#現在登録されている郵便番号は    pNumber = get_pNumber()
 @app.route("/postNumber", methods=["GET"])
 def get_pNumber():
-    #try:　　エラーが起こせないから消した
+    try:
         pNumber = request.args.get("pNumber")
         if pNumber == None:
-            return {"status":"204 No Content"}
-        
+            Object = Setting.objects().order_by("-time").first()
+            if Object == None:
+                return Response(response=json.dumps({"status":"204 No Content"}), status=204)
+
+            pNumber = Object.postnumber
+            return pNumber
+            
         #郵便番号をデータベースに保存する。
         Setting.objects.first().update(postnumber=pNumber)
-        
-        return {"status":"200 OK","pNumber":pNumber}
-
-    #except Exception as e:
-    #    logger.info(e)#エラー
-    #    
-    #    return {"status":"400 Bad Request"}
+        return Response(response=json.dumps({"status":"200 OK"}), status=200)
+    except Exception as e:
+        logger.info(e)#エラー
+        return Response(response=json.dumps({"status":"400 Bad Request"}), status=400)
