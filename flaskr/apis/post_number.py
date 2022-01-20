d6b07d94351c72ce109e36b631223ff0efce08ca
atsuki-takahashi180090 <tejc180090@gmail.com>
2022-01-20 12:14:14 +0900

    郵便番号の取得と登録

--- flaskr/apis/post_number.py
+++ flaskr/apis/post_number.py
@@ -10,24 +10,37 @@ with open("log_config.json", "r") as f:
     config.dictConfig(json.load(f))
 
 
+#現状DBがあれば動く
+#DBへの追加でのエラー
+
 #リモコンアプリからの郵便番号
-#requestじゃない呼び出し方というか、直接メソッドとして呼ばれたときにDBに保存してある郵便番号を返す
-#追加や変更は    requests.get(f"HTTP://localhost:5000/postNumber?pNumber=○○○○○○○
-#現在登録されている郵便番号は    pNumber = get_pNumber()
+#1 : 現在登録されている郵便番号の取得  ->  pNumber = get_pNumber() or requests.get(f"HTTP://localhost:5000/postNumber)
+#2 : 追加や変更  ->  requests.get(f"HTTP://localhost:5000/postNumber?pNumber=○○○○○○○)
 @app.route("/postNumber", methods=["GET"])
 def get_pNumber():
     try:
         pNumber = request.args.get("pNumber")
-        if pNumber == None:
-            Object = Setting.objects().order_by("-time").first()
-            if Object == None:
-                return Response(response=json.dumps({"status":"204 No Content"}), status=204)
+        Object = Setting.objects().order_by("-time").first()
 
+        #1 pNumberの入力なし
+        if pNumber == None:
+
+            #Object = Setting.objects().order_by("-time").first()
+
+            #DBに未保存
+            if Object == None:
+                #ページ遷移がないだけで204は出ている
+                return Response(response=json.dumps({"status":"204 No Content"}), status=204)
+            
             pNumber = Object.postnumber
             return pNumber
             
-        #郵便番号をデータベースに保存する。
-        Setting.objects.first().update(postnumber=pNumber)
+        #2 郵便番号をデータベースに保存する。
+        if Object == None:
+            Setting(postnumber=pNumber).save()
+        else:
+            Setting.objects.first().update(postnumber=pNumber)
+
         return Response(response=json.dumps({"status":"200 OK"}), status=200)
     except Exception as e:
         logger.info(e)#エラー
